98c5965cafe411e5046f412e9bf1c60eb8fcf874
Fred@Server <fredchao0618@gmail.com>
2022-04-19 01:03:35 +0800

    Add new Database Model

---
+++ LawyerUpBackend.Core/Models/Case.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LawyerUpBackend.Core.Models
+{
+    public partial class Case
+    {
+        public Case()
+        {
+            LawyerCaseMatches = new HashSet<LawyerCaseMatch>();
+        }
+
+        public int Id { get; set; }
+        public string? Type { get; set; }
+        public int? Year { get; set; }
+        public string? Word { get; set; }
+        public int? Number { get; set; }
+        public string? Classification { get; set; }
+        public string? CourtCode { get; set; }
+        public string? BeforeMain { get; set; }
+        public string? MainContent { get; set; }
+        public string? FactReason { get; set; }
+        public string? DetailUrl { get; set; }
+        public DateTime? JudgeDate { get; set; }
+
+        public virtual ICollection<LawyerCaseMatch> LawyerCaseMatches { get; set; }
+    }
+}
---
+++ LawyerUpBackend.Core/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LawyerUpBackend.Core.Models
+{
+    public partial class Test
+    {
+        public int Id { get; set; }
+        public DateTime? TestDate { get; set; }
+    }
+}
